13e42f04912554a195317a66776bfbe2637ab041
ziadAshraf7 <ziadashraf454@icloud.com>
2022-12-20 02:14:19 +0200

    ..

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query'
+
 
 const queryClient = new QueryClient()
 
@@ -20,9 +14,7 @@ const theme = createTheme({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ThemeProvider theme={theme}>
-    <QueryClientProvider client={queryClient}>
     <App />
-    </QueryClientProvider>
     </ThemeProvider>
 );
 
